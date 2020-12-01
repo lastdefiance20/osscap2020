d8ae2aa5d1cad3434704d4d9201e74fd5b436e1d
Jaeyoon Jung <lastdefiance20@gmail.com>
2020-12-01 14:52:27 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import board as bd
 import LED_display as LMD
 import threading
-import dot as dt
+import print_dot as pd
 import time
 
 #화면세팅?
@@ -36,194 +36,22 @@ def game_level():
                return 21
           else:
                print("Choose the right game level!\n")
-
-def menu_display(num):
-    if num == 1:
-        LMD.clear_pixel()
-        
-        for x in range(25):
-             for y in range(5):
-                start = dt.start()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x, y+2, 4)
-                    
-        for x in range(21):
-             for y in range(5):
-                start = dt.rule()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x, y+9, 4)
-                    
-        for x in range(7):
-             for y in range(5):
-                start = dt.right()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x+25, y+5, 4)
-        
-    else:
-        LMD.clear_pixel()
-        
-        for x in range(25):
-             for y in range(5):
-                start = dt.rank()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x+7, y+2, 4)
-                        
-        for x in range(23):
-             for y in range(5):
-                start = dt.quit()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x+7, y+9, 4)
-                    
-        for x in range(7):
-             for y in range(5):
-                start = dt.left()
-                color = start[y][x]
-                if color == 1:
-                    LMD.set_pixel(x, y+5, 4)
-                    
-def see_screen():
-    LMD.clear_pixel()
-    
-    a, b = dt.see_screen()
-    
-    for x in range(11):
-        for y in range(5):
-            color = a[y][x]
-            if color == 1:
-                    LMD.set_pixel(x+3, y+2, 4)
-    
-    for x in range(25):
-        for y in range(5):
-            color = b[y][x]
-            if color == 1:
-                    LMD.set_pixel(x+3, y+9, 4)
-    LMD.refresh()
-    
-def name():
-	LMD.clear_pixel()
-	
-	for x in range (19):
-		for y in range(7):
-			name = dt.name()
-			color = name[y][x]
-			if color == 1:
-				LMD.set_pixel(x+1, y+1,4)
-
-def P1():
-	for x in range(3):
-		for y in range(5):
-			P = dt.P()
-			color = P[y][x]
-			if color ==1:
-				LMD.set_pixel(x+1, y+10, 4)
-	
-	for x in range(3):
-		for y in range(5):
-			one = dt.one()
-			color = one[y][x]
-			if color ==1:
-				LMD.set_pixel(x+5, y+10,4)
-
-	for x in range(1):
-		for y in range(5):
-			colon = dt.colon()
-			color = colon[y][x]
-			if color ==1:
-				LMD.set_pixel(x+9,y+10,4)
-
-def P2():
-	for x in range(3):
-		for y in range(5):
-			P = dt.P()
-			color = P[y][x]
-			if color ==1:
-				LMD.set_pixel(x+17, y+10, 4)
-	
-	for x in range(3):
-		for y in range(5):
-			two = dt.two()
-			color = two[y][x]
-			if color ==1:
-				LMD.set_pixel(x+21, y+10,4)
-
-	for x in range(1):
-		for y in range(5):
-			colon = dt.colon()
-			color = colon[y][x]
-			if color ==1:
-				LMD.set_pixel(x+25,y+10,4)
-
-def level():
-	LMD. clear_pixel()
-
-	for x in range(21):
-		for y in range(7):
-			level = dt.level()
-			color = level[y][x]
-			if color ==1:
-				LMD.set_pixel(x+1,y+1,4)
-	
-	for x in range(3):
-		for y in range(5):
-			one = dt.one()
-			color = one[y][x]
-			if color ==1:
-				LMD.set_pixel(x+3, y+10,4)
-
-	for x in range(3):
-		for y in range(5):
-			two = dt.two()
-			color = two[y][x]
-			if color ==1:
-				LMD.set_pixel(x+14, y+10,4)
-	
-	for x in range(3):
-		for y in range(5):
-			three= dt.three()
-			color = three[y][x]
-			if color ==1:
-				LMD.set_pixel(x+25, y+10,4) 
-
-def P1_win():
-	LMD. clear_pixel()
-
-	for x in range(29):
-		for y in range(7):
-			P1_win = dt.P1_win()
-			color = P1_win[y][x]
-			if color ==1:
-				LMD.set_pixel(x+1,y+4,4)
-
-def P2_win():
-	LMD. clear_pixel()
-
-	for x in range(29):
-		for y in range(7):
-			P2_win = dt.P2_win()
-			color = P2_win[y][x]
-			if color ==1:
-				LMD.set_pixel(x+1,y+4,4)
     
 def run():
      LED_init()
      menu_num = 1
      while 1:
-          menu_display(menu_num)
+          pd.menu_display(menu_num)
           menu = print_menu()
           if menu ==1:
-               name()
+               pd.name()
                print("<Enter your name>\n")
-               P1()
+               pd.P1()
                Player1= input("Player1: ")
-               P2()
+               pd.P2()
                Player2= input("Player2: ")
                
-               level()
+               pd.level()
                print("\n<Game Level>\n")
                print("1. Easy")
                print("2. Normal")
@@ -258,7 +86,7 @@ def run():
                     score[words[i*3]] = [words[i*3+1], words[i*3+2]]
                    
                if symbol[0] == 5:
-                    P1_win()
+                    pd.P1_win()
                     print("P1 %s win" %Player1)
                     if Player1 in score:
                          score[Player1][0]= str(int(score[Player1][0])+1)
@@ -270,7 +98,7 @@ def run():
                          score.update({Player2: ['0','1']})	
                          
                elif symbol[1] == 5:
-                    P2_win()
+                    pd.P2_win()
                     print("P2 %s win" %Player2)
                     if Player2 in score:
                          score[Player2][0]= str(int(score[Player2][0])+1)
@@ -294,7 +122,7 @@ def run():
                time.sleep(5)
                
           elif menu ==2:
-               see_screen()
+               pd.see_screen()
                print("<게임 방법>\n")
                print("가장 최근에 길을 잃어버린 적이 있는 사람이 먼저 시작합니다.")
                print("이제부터는 주머니에서 뽑아 놓은 마법 심볼이 있는 칸에 가장 먼저 도착해야 합니다.")
@@ -312,7 +140,7 @@ def run():
                time.sleep(2)
 
           elif menu ==3:
-               see_screen()
+               pd.see_screen()
                 
                f=open("score.txt",encoding="utf-8")
                words =f.read().splitlines()
